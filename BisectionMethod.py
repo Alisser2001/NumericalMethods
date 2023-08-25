ac7675e037f1c45e4ef30633794cdf15cc19cb98
unknown <estiven.carmona@udea.edu.co>
2023-08-25 11:22:35 -0500

    Primeros métodos

---
+++ BisectionMethod.py
@@ -0,0 +1,62 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sympy import sympify
+from sympy import symbols
+from sympy import lambdify
+
+
+def fxr(xl, xu):
+    return (xl+xu)/2
+
+
+def getErr(xl, xu):
+    return abs(xu - xl)
+
+
+def BisectionMethod(f, xl, xu, stopCri, count):
+        xr = fxr(xl, xu)
+        err = getErr(xl, xu)
+        print("{:^30} {:^30} {:^30} {:^30} {:^30} {:^30} {:^30} {:^30}".format(count, xu, xl, xr, f(xu), f(xl), f(xr), round(err, 7)))
+        if f(xl)*f(xr) < 0:
+            count += 1
+            return BisectionMethod(f, xl, xr, stopCri, count)
+        if f(xl)*f(xr) > 0:
+            count += 1
+            return BisectionMethod(f, xr, xu, stopCri, count)
+        if f(xl)*f(xr) == 0 or err < stopCri:
+            return xr
+
+
+print("")
+x = symbols('x')
+fn = sympify(input('Ingresa la función: '))
+f = lambdify(x, fn)
+xl = float(input('Ingresa el valor menor de X: '))
+xu = float(input('Ingresa el valor mayor de X: '))
+stopCri = float(input('Ingresa el valor del críterio de tolerancia del error: '))
+count = 1
+print("")
+print("{:^60}".format("Método de la Bisección"))
+print("")
+print("{:^30} {:^30} {:^30} {:^30} {:^30} {:^30} {:^30} {:^30}".format("i", "xu", "xl", "xr", "f(xu)", "f(xl)", "f(xr)", "Error"))
+root = BisectionMethod(f, xl, xu, stopCri, count)
+print("Raíz: " + str(root))
+
+xpts = np.linspace(xl, xu)
+plt.plot(xpts, f(xpts))
+plt.title("Gráfica de la función " + str(fn))
+plt.axhline(color="black")
+plt.axvline(color="black")
+plt.scatter(root, 0, c="red")
+plt.scatter(xl, 0, c="blue")
+plt.scatter(xu, 0, c="blue")
+plt.annotate(round(root, 9), xy=(root, 0.5))
+plt.annotate(round(xl, 9), xy=(xl, 0.5))
+plt.annotate(round(xu, 9), xy=(xu, 0.5))
+plt.xlabel("x")
+plt.ylabel("f(x)")
+plt.grid(True, which='both')
+plt.ylim([-15, 15])
+plt.show()
+
+
---
+++ CuadraticInterpolation.py
@@ -0,0 +1,74 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sympy import sympify
+from sympy import symbols
+from sympy import lambdify
+
+def getErr(x1, x3):
+    return abs(x1 - x3)
+
+
+def x3Num(y0, y1, y2, x0, x1, x2):
+    return (y0 * (x1 ** 2 - x2 ** 2) + y1 * (x2 ** 2 - x0 ** 2) + y2 * (x0 ** 2 - x1 ** 2))
+
+
+def x3Den(y0, y1, y2, x0, x1, x2):
+    return (2 * y0 * (x1 - x2) + 2 * y1 * (x2 - x0) + 2 * y2 * (x0 - x1))
+
+
+def fx3(y0, y1, y2, x0, x1, x2):
+    return (x3Num(y0, y1, y2, x0, x1, x2)/x3Den(y0, y1, y2, x0, x1, x2))
+
+def CuadraticInterpolation(f, x0, x1, x2, stopCri, count):
+    y0 = f(x0)
+    y1 = f(x1)
+    y2 = f(x2)
+    x3 = fx3(y0, y1, y2, x0, x1, x2)
+    y3 = f(x3)
+    err = getErr(x1, x3)
+    print("{:^30} {:^30} {:^30} {:^30} {:^30} {:^30} {:^30} {:^30} {:^30} {:^30}".format(count, x0, x1, x2, x3, y0, y1, y2, y3, err))
+
+    if y3 > y1:
+        count += 1
+        return CuadraticInterpolation(f, x1, x3, x2, stopCri, count)
+    if y3 < y1:
+        count += 1
+        return CuadraticInterpolation(f, x0, x3, x1, stopCri, count)
+    if y3 == y1 or err < stopCri:
+        return y3
+
+
+print("")
+x = symbols('x')
+fn = sympify(input('Ingresa la función: '))
+f = lambdify(x, fn)
+x0 = float(input('Ingresa el valor menor de X: '))
+x1 = float(input('Ingresa el valor medio de X: '))
+x2 = float(input('Ingresa el valor mayor de X: '))
+stopCri = float(input('Ingresa el valor del críterio de tolerancia del error: '))
+count = 1
+print("")
+print("{:^60}".format("Método de la Interpolación Cuadrática"))
+print("")
+print("{:^30} {:^30} {:^30} {:^30} {:^30} {:^30} {:^30} {:^30} {:^30} {:^30}".format("i", "x0", "x1", "x2", "x3", "f(x0)", "f(x1)", "f(x2)", "f(x3)", "Error"))
+max = CuadraticInterpolation(f, x0, x1, x2, stopCri, count)
+print("Raíz: " + str(max))
+
+xpts = np.linspace(x0, x2)
+plt.plot(xpts, f(xpts))
+plt.title("Gráfica de la función " + str(fn))
+plt.axhline(color="black")
+plt.axvline(color="black")
+plt.scatter(max, 0, c="red")
+plt.scatter(x0, 0, c="blue")
+plt.scatter(x1, 0, c="blue")
+plt.scatter(x2, 0, c="blue")
+plt.annotate(round(max, 9), xy=(max, 0.5))
+plt.annotate(round(x0, 9), xy=(x0, 0.5))
+plt.annotate(round(x1, 9), xy=(x1, 0.5))
+plt.annotate(round(x2, 9), xy=(x2, 0.5))
+plt.xlabel("x")
+plt.ylabel("f(x)")
+plt.grid(True, which='both')
+plt.ylim([-15, 15])
+plt.show()
