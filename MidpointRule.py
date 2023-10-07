645fc9c3ea9e1cce6679801c1a39aae94d1bfa88
unknown <estiven.carmona@udea.edu.co>
2023-10-07 16:50:41 -0500

    Regla del punto medio completa

---
+++ MidpointRule.py
@@ -0,0 +1,50 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sympy import sympify
+from sympy import symbols
+from sympy import lambdify
+
+def MidPoint(f, fn, a, b, n):
+    h = (b - a) / n
+    s = 0
+    for i in range(0, n):
+        xi = a + (i + 0.5) * h
+        s += f(xi)
+        print("{:^30} {:^30} {:^30} {:^30}".format(str(i + 1), str(xi), str(f(xi)), str(s*h)))
+    return s*h
+
+def Solution():
+    print("")
+    x = symbols('x')
+    fn = sympify(input('Ingresa la función: '))
+    f = lambdify(x, fn)
+    xl = float(input('Ingresa el valor del límite inferior: '))
+    xu = float(input('Ingresa el valor dle límite superior: '))
+    n = int(input('Ingresa el valor de los sub-intervalos: '))
+    print("")
+    print("{:^120}".format("Método del Trapecio Compuesto"))
+    print("")
+    print("{:^30} {:^30} {:^30} {:^30}".format("i", "Xi", "f(Xi)", "Area Total"))
+    print("")
+    print("\nEl valor aprox. De la integral definida es: ", MidPoint(f, fn, xl, xu, n))
+    print("Para h = " + str(((xu - xl) / n)) + " y f(x) = " + str(fn))
+    print("Entre a = " + str(xl) + " y b = " + str(xu))
+    print("Tomando " + str(n) + " sub-intervalos.")
+    plt.figure(figsize=(8, 6))
+    xpts = np.linspace(xl - 10, xu + 10)
+    ypts = f(xpts)
+    plt.plot(xpts, ypts)
+    plt.title("Gráfica de la función " + str(fn))
+    plt.axhline(color="black")
+    plt.axvline(color="black")
+    plt.scatter(xl, f(xl), c="red")
+    plt.scatter(xu, f(xu), c="red")
+    plt.annotate((xl, f(xl)), xy=(xl, f(xl)))
+    plt.annotate((xu, f(xu)), xy=(xu, f(xu)))
+    plt.fill_between(xpts, ypts, where=[(x >= xl) and (x <= xu) for x in xpts], alpha=0.3, color='gray', label="Área")
+    plt.xlabel("x")
+    plt.ylabel("f(x)")
+    plt.grid(True, which='both')
+    plt.show()
+
+Solution()
