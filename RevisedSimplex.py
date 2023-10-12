3b539e2683f3a296e2f17203bcc31e10bfd17abe
unknown <estiven.carmona@udea.edu.co>
2023-10-11 19:33:05 -0500

    Simplex Revisado - Construcción matricial

--- RevisedSimplex.py
+++ RevisedSimplex.py
@@ -14,8 +14,11 @@ def Solution():
                 input("Ingresa el valor del coeficiente de X" + str(j + 1) + " en la restricción " + str(i + 1) + ": "))
             coefsRest.append((i, "X" + str(j + 1), coef))
     typeRest = []
+    slackVar = 0
     for i in range(0, numRest):
         t = input("Ingresa el tipo de la restriccición " + str(i + 1) + " (>=, ==, <=): ")
+        if t == "<=" or  t == ">=":
+            slackVar += 1
         typeRest.append(t)
     right = []
     for i in range(0, numRest):
@@ -33,5 +36,47 @@ def Solution():
         Rest[i] = Rest[i] + " " + typeRest[i] + " " + str(right[i])
     for i in range(0, len(Rest)):
         print("Rest " + str(i+1) + ": " + Rest[i])
+    print("\nForma Estandar: ")
+    for i in range(0, slackVar):
+        Z = Z + " + (0)" + "S" + str(i+1)
+    print(" ")
+    print(obj + " Z = " + Z)
+    Rest = ["" for _ in range(numRest)]
+    for i in range(0, len(coefsRest)):
+        Rest[coefsRest[i][0]] = Rest[coefsRest[i][0]] + "(" + str(coefsRest[i][2]) + ")" + coefsRest[i][1] + (" + " if int(coefsRest[i][1][1]) < numVars else "")
+    for i in range(0, len(Rest)):
+        if typeRest[i] == ">=":
+            Rest[i] = Rest[i] + " - (1.0)S" + str(i+1)
+        if typeRest[i] == "<=":
+            Rest[i] = Rest[i] + " + (1.0)S" + str(i+1)
+    for i in range(0, len(Rest)):
+        Rest[i] = Rest[i] + " == " + str(right[i])
+    for i in range(0, len(Rest)):
+        print("Rest " + str(i + 1) + ": " + Rest[i])
+    C = coefsZ
+    for i in range(0, slackVar):
+        C.append(0)
+    B = []
+    for i in range(0, len(right)):
+        B.append([right[i]])
+    X = []
+    for i in range(0, numVars):
+        X.append(["X" + str(i+1)])
+    for i in range(0, slackVar):
+        X.append(["S" + str(i+1)])
+    P = [[] for _ in range(numVars)]
+    for i in range(0, len(coefsRest)):
+        P[coefsRest[i][0]].append(coefsRest[i][2])
+    for i in range(0, len(P)):
+        s = [0 for _ in range(0, slackVar)]
+        if typeRest[i] == ">=":
+            s[i] = -1
+        if typeRest[i] == "<=":
+            s[i] = 1
+        P[i] = P[i] + s
+
+
+
+
 
 Solution()
